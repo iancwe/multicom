67d237cd9bc20783d95723cd18e4d3d67ff13243
iancwe <crubbishhute@gmail.com>
2017-07-19 20:15:20 +0800

    fixing host website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
+  # def background_image_url
+  #   Company.current.img_url
+  # end
+
   def background_image_url
-    Company.current.img_url
+    if Company.current.img_url == ''
+      puts "No tenant branding found"
+      puts "****************************************"
+    elsif true
+    else
+      puts Company.current.img_url
+      puts "****************************************"
+      Company.current.img_url
+    end
   end
 end
